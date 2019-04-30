ce3cf45bf5f91e8c5caa4f4bc7a9382b62196d08
Sydney Baffour <sydney.baffour@WL-204-124.WPA.Claremont.Edu>
2019-04-29 21:20:01 -0700

    Saving progress.

---
+++ Enemy.cpp
@@ -0,0 +1,82 @@
+#include "Enemy.hpp"
+
+namespace QuickSDL {
+Enemy::Enemy(float x)
+{
+    
+    mMoveSpeed = x; 
+    mMoveBounds = Vector2(0.0f, 800.0f);
+
+    mTimer = Timer::Instance();
+    //mCurrentState = incoming;
+    
+
+    mTexture = new Texture("");
+    mTexture->Parent(this);
+    mTexture->Pos(VEC2_ZERO);
+    mAlive = 1; 
+}
+
+//deconstructor
+Enemy::~Enemy()
+{
+    mTimer = NULL;
+    delete mTexture;
+    mTexture = NULL;
+}
+bool Enemy::alive()
+{
+    return mAlive=1; 
+}
+bool Enemy::hasCollided(int pos)
+{
+    return false;
+}
+
+void Enemy::kill()
+
+{
+    
+}    
+
+void Enemy::changeSpeed(float x){
+    mMoveSpeed =x; 
+}
+
+// void Enemy:: Render(){
+
+// }
+void Enemy::Update()
+{
+    if (alive())
+    {
+        mTexture->Render();
+        for (int i = 0; i < Graphics::Instance()->SCREEN_WIDTH; i++)
+        {
+            Translate(-VEC2_RIGHT * mMoveSpeed * mTimer->DeltaTime);
+        }
+        mAlive= false; 
+    }
+    kill();
+} 
+}
+
+// void Enemy:: HandleIncomingState(){
+
+// }
+
+// //void Enemy:: HandleDeadState(){}
+// //void Enemy:: HandleStates(){
+
+//     switch(mCurrentState){
+
+//         case incoming;
+//         HandleIncomingState();
+//         break;
+
+//         case dead;
+//         HandleDeadState();
+//         break;
+
+//     }
+// }
